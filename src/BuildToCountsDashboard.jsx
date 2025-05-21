ff83da16336fe4f7680674d4ea4c65e93559bd03
Jonathan Brackett <brackettj@fsi-stl.com>
2025-05-21 07:19:05 -0400

    Initial dashboard commit

---
+++ src/BuildToCountsDashboard.jsx
@@ -0,0 +1,1 @@
+export default function BuildToCountsDashboard() { return <div>Dashboard placeholder</div>; }
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import BuildToCountsDashboard from './BuildToCountsDashboard';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BuildToCountsDashboard />
+  </React.StrictMode>
+);
